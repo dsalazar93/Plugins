e375bf589e8587468f6e120ce7f3c4433bfc4a69
Juan Pablo Baena Zuluaga <Juan Pablo Baena Zuluaga>
2016-11-11 18:23:13 -0500

    Organizando código

--- drag-and-drop/js/jquery.drag-and-drop.js
+++ drag-and-drop/js/jquery.drag-and-drop.js
@@ -19,49 +19,42 @@
 			elems.container = $this.addClass('container lnt-dnd-container');
 			elems.row = $('<div>', {Class: 'row'}).appendTo(elems.container);
 			
-			elems.right = $('<div>', { Class: 'col-md-6 lnt-dnd-right'}).appendTo(elems.row);
 			elems.left = $('<div>', { Class: 'col-md-6 lnt-dnd-left'}).appendTo(elems.row);
+			elems.right = $('<div>', { Class: 'col-md-6 lnt-dnd-right'}).appendTo(elems.row);
 			
+			elems.ulLeft = $('<ul>').appendTo(elems.left).sortable({ revert: true });
 			elems.ulRight = $('<ul>').appendTo(elems.right);
-			elems.ulLeft = $('<ul>', { Class: 'sortable'}).appendTo(elems.left).sortable({ revert: true });
 
-			options.drags.forEach(function(elem){		
-
+			options.drags.forEach(function(elem){
 				var li = $('<li>', { Class: 'lnt-content-middle draggable'})  
 							.appendTo(elems.ulLeft)
 							.html(elem.text)
-							.data('reference', elem.reference)
-							// .draggable({ revert: true });
-
+							.data('reference', elem.reference);
 			});
 
 			options.drops.forEach(function(elem){
-				var li = $('<li>', { Class: 'lnt-content-middle lnt-dnd-hover'})
+				var li = $('<li>', { Class: 'lnt-content-middle'})
 							.appendTo(elems.ulRight)
 							.html('<div>' + elem.text + '</div>')
 							.droppable({
+								hoverClass: "ui-drop-hover",
 								drop: function(evt, origin) {
-									
-									    // ev.preventDefault();
-									    // var data = ev.dataTransfer.getData("text");
-									    // ev.target.appendChild(document.getElementById(data));
+									var $origin = $(origin.helper)
+										,$this = $(this);
 
-									var $origin = $(origin.helper);
-									if ($origin.data('reference') == elem.id) {
-										/**
-										 * Remueve cajas de la derecha cuando hacemos drop sobre el drag
-										 */
-										$origin.remove();
+										$origin.clone()
+											   .removeClass('draggable')
+											   .removeClass('ui-sortable-helper')
+											   .css({
+													left : '1em',
+													top: '1em',
+													display: 'inline-block',
+													width: 'auto',
+													padding: '1em'
+												})
+											.appendTo($this)
 
-
-										if(elem.popupID)
-											$.popup({ id: elem.popupID});
-
-										if(elem.popuptext)
-											$.popup(elem.popuptext);
-
-
-									}
+										$origin.css('display', 'none');
 								}
 							})
 			});
