42d15b39d4a6ba665ca869f2cf1f78252b853ec7
carlosmnarvaez <cpromultimedia@gmail.com>
2016-11-17 11:13:41 -0500

    Modificaciones varias plugin DragAndDrop

--- drag-and-drop/js/jquery.drag-and-drop.js
+++ drag-and-drop/js/jquery.drag-and-drop.js
@@ -12,6 +12,8 @@
 
 		var options = $.extend({}, defaults, opts);
 
+		var flag = true;
+
 		return this.each(function(){
 			var $this = $(this);
 
@@ -19,47 +21,82 @@
 			elems.container = $this.addClass('container lnt-dnd-container');
 			elems.row = $('<div>', {Class: 'row'}).appendTo(elems.container);
 			
-			elems.left = $('<div>', { Class: 'col-md-6 lnt-dnd-left'}).appendTo(elems.row);
-			elems.right = $('<div>', { Class: 'col-md-6 lnt-dnd-right'}).appendTo(elems.row);
+			elems.left = $('<div>', { Class: 'col-md-3 lnt-dnd-left'}).appendTo(elems.row);
+			elems.right = $('<div>', { Class: 'col-md-9 lnt-dnd-right'}).appendTo(elems.row);
 			
-			elems.ulLeft = $('<ul>').appendTo(elems.left).sortable({ revert: true });
+			elems.ulLeft = $('<ul>').appendTo(elems.left).sortable({ revert: flag });
 			elems.ulRight = $('<ul>').appendTo(elems.right);
 
 			options.drags.forEach(function(elem){
 				var li = $('<li>', { Class: 'lnt-content-middle draggable'})  
 							.appendTo(elems.ulLeft)
 							.html(elem.text)
-							.data('reference', elem.reference);
+							.data('reference', elem.reference)
+							// .draggable( {revert: true } );
+														
 			});
 
 			options.drops.forEach(function(elem){
-				var li = $('<li>', { Class: 'lnt-content-middle'})
+				var li = $('<li>', { Class: 'lnt-content-middle droppable'})
 							.appendTo(elems.ulRight)
 							.html('<div>' + elem.text + '</div>')
 							.droppable({
 								hoverClass: "ui-drop-hover",
 								drop: function(evt, origin) {
+									flag = false;
 									var $origin = $(origin.helper)
 										,$this = $(this);
 
+										if ($origin.data('clone')) {
+											$this.append($origin);
+											return;
+										}
+
 										$origin.clone()
+											   .draggable({ revert: true })
 											   .removeClass('draggable')
 											   .removeClass('ui-sortable-helper')
-											   .css({
-													left : '1em',
-													top: '1em',
-													display: 'inline-block',
-													width: 'auto',
-													padding: '1em'
-												})
-											.appendTo($this)
+											   .addClass('lnt-css-drag')
+											 //   .css({
+												// 	left : '-142px',
+												// 	top: '-11px',
+												// 	display: 'inline-block',
+												// 	width: '100%',	
+												// 	position: 'absolute',
+												// 	background: '#ccc',									
+												// 	padding: '8px'
 
-										$origin.css('display', 'none');
+												// })
+												.appendTo($this)
+												.on('click', function() {
+													$(this).remove();
+														$origin.css('display', 'block');
+												})
+												.data('clone', true)
+
+										$origin.css('display', 'none')
+
+
+
+									// if ($origin.data('reference') == elem.id) {
+									// 	$origin.remove();
+
+									// 	if(elem.popupID)
+									// 		$.popup({ id: elem.popupID});
+
+									// 	if(elem.popuptext)
+									// 		$.popup(elem.popuptext);
+									// }
+
 								}
 							})
-			});
 
+
+			});
 		});
 	}
 
 })(jQuery)
+
+//--------------------------------------------Revertir drag and drop--------------------------------------------------//
+//http://devilmaycode.altervista.org/revert-a-jquery-draggable-object-back-to-its-original-container-on-out-event-of-d/
