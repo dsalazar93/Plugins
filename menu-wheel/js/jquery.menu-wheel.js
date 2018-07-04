3d18d452dc026b09541a44744930942708c656e5
jpbaena13 <jpbaena13@gmail.com>
2018-07-03 19:19:15 -0500

    Añadiendo Menu-wheel

---
+++ menu-wheel/js/jquery.menu-wheel.js
@@ -0,0 +1,64 @@
+;(function($, undefined){
+
+    $.fn.menuWheel = function(opt) {
+
+        var options = {
+            numBtns: 10,
+            radius: 150,
+        }
+
+        options = $.extend({}, options, opt);
+
+        this.each(function(){
+            var $this = $(this);
+
+            if ($this.data('num-btns'))
+                options.numBtns = $this.data('num-btns');
+
+            if ($this.data('radius'))
+                options.radius = $this.data('radius');
+            
+            $this.css({ 
+                width: options.radius*2, 
+                height: options.radius*2,
+            });
+
+            var delta = Math.PI*2/(options.numBtns + 1);
+            var rds = options.radius/8;
+
+            var _events = {
+                init: function() {
+                    for(var i = 0; i < options.numBtns; i++) {
+                        var theta = delta*i;
+                        var btn = $('<div>', {Class: 'unc-menu-wheel-btn'})
+                                    .html(i)
+                                    .data('theta', theta)
+                                    .data('idx', i)
+                                    .appendTo($this);
+                        
+                        btn.css({
+                            width: rds*2,
+                            height: rds*2,
+                            top: 'calc(50% - ' + (options.radius-rds*1.5)*Math.sin(theta + Math.PI/2) + 'px)',
+                            left: 'calc(50% - ' + (options.radius-rds*1.5)*Math.cos(theta + Math.PI/2) + 'px)',
+                            transform: 'translate(-50%, -50%) rotate(' + (-(Math.PI/2)+theta + Math.PI/2) + 'rad)'
+                        });
+
+                        btn.on('click', function() {
+                            var _$this = $(this);
+
+                            options.btnFunctions && options.btnFunctions[_$this.data('idx')]
+                                    && options.btnFunctions[_$this.data('idx')]()
+
+                            $this.css('transform', 'translateY(70%) rotate(-' + _$this.data('theta') + 'rad)');
+                        })
+                    }
+                }
+            }
+
+            _events.init();
+        });
+
+    }
+
+})(jQuery)
